7aeb7a0f30905b60cea8a1746146c9c8fb2afcab
Karup0 <l20212929@tectijuana.edu.mx>
2025-05-17 16:50:56 -0700

    Actualizacion de seguridad de la informacion de los datos de la aplicacion y actualizacion de los paquetes para la aplicacion

--- backend/pokemon_tracker/settings.py
+++ backend/pokemon_tracker/settings.py
@@ -9,9 +9,24 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import os
+
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
